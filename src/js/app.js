182e0a9a4772922ba33b787d3f2ea6e8beffcc31
Trevor Sargent <tbird1710@gmail.com>
2017-08-27 18:43:18 -0700

    add p5

---
+++ src/js/app.js
@@ -0,0 +1,25 @@
+import p5 from 'p5'
+
+
+
+const sketch = (p) => {
+	let gray = 0
+
+	p.setup = () => {
+		let cW = p.windowWidth
+		let cH = p.windowHeight
+		p.createCanvas(cW, cH)
+	}
+
+	p.draw = () => {
+		p.background(gray)
+		p.rect(p.width / 2 - 25, p.height / 2 - 25, 50, 50)
+	}
+
+	p.mousePressed = () => {
+		gray = (gray + 16) % 256
+	}
+}
+
+// See https://github.com/processing/p5.js/wiki/Instantiation-Cases
+new p5(sketch) // 2nd param can be a canvas html element
