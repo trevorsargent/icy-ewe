d3c5a55c64683d8c9a11bc9b26a72f2bbfc334eb
Trevor Sargent <tbird1710@gmail.com>
2017-08-29 19:39:02 -0700

    hello world

--- src/js/app.js
+++ src/js/app.js
@@ -3,21 +3,21 @@ import p5 from 'p5'
 const sketch = (p) => {
 	let gray = 0
 
+	let cW = p.windowWidth
+	let cH = p.windowHeight
+
 	p.setup = () => {
-		let cW = p.windowWidth
-		let cH = p.windowHeight
 		p.createCanvas(cW, cH)
 	}
 
 	p.draw = () => {
-		p.background(gray)
-		p.rect(p.width / 2 - 25, p.height / 2 - 25, 50, 50)
+		p.background(255)
+		p.textSize(20)
+		p.text("Icy Ewe", cW / 2 - 25, cH / 2)
 	}
 
 	p.mousePressed = () => {
-		gray = (gray + 16) % 256
 	}
 }
 
-// See https://github.com/processing/p5.js/wiki/Instantiation-Cases
 new p5(sketch) // 2nd param can be a canvas html element
