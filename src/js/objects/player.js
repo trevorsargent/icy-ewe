d2fbea0ba46decb6d6182da433d16118fdc2eadd
Trevor Sargent <tbird1710@gmail.com>
2017-09-01 14:13:55 -0700

    normalized walk velocity

--- src/js/objects/player.js
+++ src/js/objects/player.js
@@ -1,4 +1,4 @@
-import { createVector, add, dist } from '../math/vector.js'
+import { createVector, add, dist, normalize, scale, magnitude } from '../math/vector.js'
 import { cW, cH, WALKVELOCITY } from '../lib/constants.js'
 import { detectCollisions, applyCollisions, isNear } from '../math/physics.js'
 
@@ -14,6 +14,8 @@ export const updatePlayer = (player, obstacles, keys) => {
 		vel = applyCollisions(vel, collisions)
 	})
 
+	vel = normalize(vel)
+	vel = scale(vel, WALKVELOCITY)
 	player.vel = vel
 
 	player.pos = add(player.pos, player.vel)
