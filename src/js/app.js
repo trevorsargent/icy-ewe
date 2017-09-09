8027eb71a13d908b5ab0e50b3fd71fc38759d82e
Kassmanben <Kassmanben@gmail.com>
2017-09-08 17:22:20 -0700

    added make-a-house function

--- src/js/app.js
+++ src/js/app.js
@@ -2,8 +2,8 @@ import p5 from 'p5'
 import { newPlayer, updatePlayer } from './objects/player.js'
 import { cW, cH, KEYS } from './lib/constants.js'
 import { newRandomObstacle } from './objects/obstacle.js'
-import { wallPath } from './world/walls.js'
 import { createVector, magnitude } from './math/vector.js'
+import { createHouse } from './objects/house.js'
 
 const sketch = (p) => {
 
@@ -83,6 +83,41 @@ const sketch = (p) => {
 		p.pop()
 	}
 
+	const drawHouse = (c, h, w, entSide) => {
+		const ul = createVector(c.x - (w / 2), c.y - (h / 2))
+		const ur = createVector(c.x + (w / 2), c.y - (h / 2))
+		const ll = createVector(c.x - (w / 2), c.y + (h / 2))
+		const lr = createVector(c.x + (w / 2), c.y + (h / 2))
+		switch (entSide) {
+			case "left":
+				if (ll.y - ul.y > player.height) {
+					const start = createVector(c.x - (w / 2), c.y - (player.height * .7))
+					const end = createVector(c.x - (w / 2), c.y + (player.height * .7))
+					console.log(end.y - start.y)
+					return wallPath([start, ul, ur, lr, ll, end])
+				}
+			case "right":
+				if (lr.y - ur.y > player.height) {
+					const start = createVector(c.x + (w / 2), c.y + (player.height * .7) + 2)
+					const end = createVector(c.x + (w / 2), c.y - (player.height * .7) - 2)
+					return wallPath([start, lr, ll, ul, ur, end])
+				}
+			case "up":
+				if (ur.x - ul.x > player.width) {
+					const start = createVector(c.x + (player.height * .7) + 2, c.y - (h / 2))
+					const end = createVector(c.x - (player.height * .7) - 2, c.y - (h / 2))
+					return wallPath([start, ur, lr, ll, ul, end])
+				}
+			case "down":
+				if (lr.x - ll.x > player.width) {
+					const start = createVector(c.x - (player.height * .7) - 2, c.y + (h / 2))
+					const end = createVector(c.x + (player.height * .7) + 2, c.y + (h / 2))
+					return wallPath([start, ll, ul, ur, lr, end])
+				}
+		}
+		console.log("DIDNT WORK")
+	}
+
 	p.setup = () => {
 		player = newPlayer()
 		player = addSkin(player)
@@ -95,13 +130,7 @@ const sketch = (p) => {
 		obstacles.push(newRandomObstacle(player))
 		obstacles.push(newRandomObstacle(player))
 
-		const A = createVector(200, 100)
-		const B = createVector(200, 300)
-		const C = createVector(100, 200)
-		const D = createVector(300, 200)
-		obstacles = obstacles.concat(wallPath([A, B]))
-		obstacles.push.apply(obstacles, wallPath([A, B]))
-		obstacles = obstacles.concat(wallPath([C, D]))
+		obstacles = obstacles.concat(drawHouse(createVector(10, 10), 300, 500, "up"))
 	}
 
 	p.draw = () => {
