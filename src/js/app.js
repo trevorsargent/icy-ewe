26938d386dbec951f66cf616c32aed52adc4c20f
Trevor Sargent <tbird1710@gmail.com>
2017-09-14 14:40:45 -0700

    move draw and skin definitions to player.js - INJECT ALL THE DEPENDENCIES

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,12 @@
 import p5 from 'p5'
-import { newPlayer, updatePlayer } from './objects/player.js'
+import { newPlayer, updatePlayer, makeDrawPlayer, makeAddPlayerSkin } from './objects/player.js'
 import { cW, cH, KEYS } from './lib/constants.js'
-import { newRandomObstacle } from './objects/obstacle.js'
 import { newRandomTreat } from './objects/treat.js'
-import { wallPath } from './world/walls.js'
-import { createVector, magnitude } from './math/vector.js'
+import { createVector } from './math/vector.js'
 import { drawHouse } from './objects/islandmap.js'
 
 const sketch = (p) => {
 
-	let playerColor = [0, 0, 128]
 	let obsColor = [139, 69, 19]
 	let treatColor = [255, 0, 0]
 
@@ -18,27 +15,8 @@ const sketch = (p) => {
 	let player = {}
 	let initialTreatCount = 0
 
-	const addSkin = (player) => {
-		player.skin.walk.right[0] = p.loadImage('./src/textures/player/walk/right-0.jpg')
-		player.skin.walk.right[1] = p.loadImage('./src/textures/player/walk/right-1.jpg')
-		player.skin.walk.right[2] = p.loadImage('./src/textures/player/walk/right-2.jpg')
-		player.skin.walk.right[3] = p.loadImage('./src/textures/player/walk/right-3.jpg')
-		player.skin.walk.right[4] = p.loadImage('./src/textures/player/walk/right-4.jpg')
-		player.skin.walk.right[5] = p.loadImage('./src/textures/player/walk/right-5.jpg')
-		player.skin.walk.right[6] = p.loadImage('./src/textures/player/walk/right-6.jpg')
-		player.skin.walk.right[7] = p.loadImage('./src/textures/player/walk/right-7.jpg')
-		player.skin.walk.right[8] = p.loadImage('./src/textures/player/walk/right-8.jpg')
-		player.skin.walk.left[0] = p.loadImage('./src/textures/player/walk/left-0.jpg')
-		player.skin.walk.left[1] = p.loadImage('./src/textures/player/walk/left-1.jpg')
-		player.skin.walk.left[2] = p.loadImage('./src/textures/player/walk/left-2.jpg')
-		player.skin.walk.left[3] = p.loadImage('./src/textures/player/walk/left-3.jpg')
-		player.skin.walk.left[4] = p.loadImage('./src/textures/player/walk/left-4.jpg')
-		player.skin.walk.left[5] = p.loadImage('./src/textures/player/walk/left-5.jpg')
-		player.skin.walk.left[6] = p.loadImage('./src/textures/player/walk/left-6.jpg')
-		player.skin.walk.left[7] = p.loadImage('./src/textures/player/walk/left-7.jpg')
-		player.skin.walk.left[8] = p.loadImage('./src/textures/player/walk/left-8.jpg')
-		return player;
-	}
+	const addPlayerSkin = makeAddPlayerSkin(p)
+	const drawPlayer = makeDrawPlayer(p)
 
 	const drawObstacle = (obstacle) => {
 		p.push()
@@ -77,34 +55,6 @@ const sketch = (p) => {
 		p.pop()
 	}
 
-	const drawPlayer = (player) => {
-		let walkCycle = 0;
-		let img;
-		if (magnitude(player.vel) > 0) {
-			walkCycle = (Math.floor(p.frameCount / 2) % 9)
-			if (player.vel.x > 0) {
-				img = player.skin.walk.right[walkCycle];
-				player.prevDirection = "right"
-			} else {
-				img = player.skin.walk.left[walkCycle];
-				player.prevDirection = "left"
-			}
-		} else {
-			if (player.prevDirection == "left") {
-				img = player.skin.walk.left[0]
-			}
-			else {
-				img = player.skin.walk.right[0]
-			}
-		}
-
-
-		p.image(img, player.pos.x - player.width / 2, player.pos.y - player.height / 2)
-		p.push()
-		p.fill(playerColor)
-		p.pop()
-	}
-
 	const drawInventory = (player, treatCount) => {
 		p.fill("black")
 		p.beginShape();
@@ -130,7 +80,7 @@ const sketch = (p) => {
 
 	p.setup = () => {
 		player = newPlayer()
-		player = addSkin(player)
+		player = addPlayerSkin(player)
 		p.createCanvas(cW, cH)
 		p.noStroke()
 		obstacles = obstacles.concat(drawHouse(createVector(10, 10), 300, 500, "up", player))
@@ -141,6 +91,8 @@ const sketch = (p) => {
 		treats = treats.concat(newRandomTreat(player))
 		initialTreatCount = 5
 	}
+
+
 
 	p.draw = () => {
 		p.background(255)
@@ -153,7 +105,6 @@ const sketch = (p) => {
 		obstacles.forEach(e => {
 			drawObstacle(e)
 		})
-		let treatCount = treats.length
 		treats = treats.filter(f => {
 			return !f.pickedUp
 		})
--- src/js/objects/player.js
+++ src/js/objects/player.js
@@ -1,6 +1,52 @@
-import { createVector, add, dist, normalize, scale } from '../math/vector.js'
+import { createVector, add, magnitude, normalize, scale } from '../math/vector.js'
 import { cW, cH, WALKVELOCITY } from '../lib/constants.js'
 import { detectCollisions, applyCollisions, isNear } from '../math/physics.js'
+
+export const makeDrawPlayer = (p) => (player) => {
+	let walkCycle = 0;
+	let img;
+	if (magnitude(player.vel) > 0) {
+		walkCycle = (Math.floor(p.frameCount / 2) % 9)
+		if (player.vel.x > 0) {
+			img = player.skin.walk.right[walkCycle];
+			player.prevDirection = "right"
+		} else {
+			img = player.skin.walk.left[walkCycle];
+			player.prevDirection = "left"
+		}
+	} else {
+		if (player.prevDirection == "left") {
+			img = player.skin.walk.left[0]
+		}
+		else {
+			img = player.skin.walk.right[0]
+		}
+	}
+
+	p.image(img, player.pos.x - player.width / 2, player.pos.y - player.height / 2)
+}
+
+export const makeAddPlayerSkin = (p) => (player) => {
+	player.skin.walk.right[0] = p.loadImage('./src/textures/player/walk/right-0.jpg')
+	player.skin.walk.right[1] = p.loadImage('./src/textures/player/walk/right-1.jpg')
+	player.skin.walk.right[2] = p.loadImage('./src/textures/player/walk/right-2.jpg')
+	player.skin.walk.right[3] = p.loadImage('./src/textures/player/walk/right-3.jpg')
+	player.skin.walk.right[4] = p.loadImage('./src/textures/player/walk/right-4.jpg')
+	player.skin.walk.right[5] = p.loadImage('./src/textures/player/walk/right-5.jpg')
+	player.skin.walk.right[6] = p.loadImage('./src/textures/player/walk/right-6.jpg')
+	player.skin.walk.right[7] = p.loadImage('./src/textures/player/walk/right-7.jpg')
+	player.skin.walk.right[8] = p.loadImage('./src/textures/player/walk/right-8.jpg')
+	player.skin.walk.left[0] = p.loadImage('./src/textures/player/walk/left-0.jpg')
+	player.skin.walk.left[1] = p.loadImage('./src/textures/player/walk/left-1.jpg')
+	player.skin.walk.left[2] = p.loadImage('./src/textures/player/walk/left-2.jpg')
+	player.skin.walk.left[3] = p.loadImage('./src/textures/player/walk/left-3.jpg')
+	player.skin.walk.left[4] = p.loadImage('./src/textures/player/walk/left-4.jpg')
+	player.skin.walk.left[5] = p.loadImage('./src/textures/player/walk/left-5.jpg')
+	player.skin.walk.left[6] = p.loadImage('./src/textures/player/walk/left-6.jpg')
+	player.skin.walk.left[7] = p.loadImage('./src/textures/player/walk/left-7.jpg')
+	player.skin.walk.left[8] = p.loadImage('./src/textures/player/walk/left-8.jpg')
+	return player;
+}
 
 export const updatePlayer = (player, obstacles, treats, keys) => {
 	obstacles = obstacles.filter(f => {
